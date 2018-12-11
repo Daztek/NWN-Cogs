f08b736fef52f625ae3444fe0c1c0dd26318a919
Daztek <daztek@gmail.com>
2018-12-11 21:44:34 +0100

    Redis Sub Test

--- nwnpublish/nwnpublish.py
+++ nwnpublish/nwnpublish.py
@@ -1,5 +1,8 @@
 import discord
+import aioredis
+import asyncio
 from discord.ext import commands
+from itertools import count
 
 class NWNPublish:
     """My custom cog that does stuff!"""
@@ -14,5 +17,15 @@ class NWNPublish:
         #Your code will go here
         await self.bot.say("Publishing...!")
 
+    async def reader(ch):
+        async for msg in ch.iter(encoding='utf-8'):
+            await self.bot.say("Message: " + msg)
+
 def setup(bot):
+    sub = await aioredis.create_redis('redis://localhost')
+
+    channel = await sub.subscribe('test')
+
+    tsk = asyncio.ensure_future(reader(channel))
+
     bot.add_cog(NWNPublish(bot))
